29814ad279835d0b1f08117bbf0f0276bc6b15e4
vernonkjr <vkerrjr@gmail.com>
2020-05-30 12:43:48 -0400

    add stubs for Home, Rooms, SingleRoom and Error pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
 function App() {
   return (
-    <>hello from app</>
+    <>
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <Error />
+    </>
   );
 }
 
---
+++ src/pages/Rooms.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Rooms = () => {
+    return (
+        <div>
+            hello from rooms page
+        </div>
+    )
+}
+
+export default Rooms
+
+
