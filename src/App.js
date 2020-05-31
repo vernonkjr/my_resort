5c14f98f3291415b4cd5158435ab8bdea3bbc584
vernonkjr <vkerrjr@gmail.com>
2020-05-31 15:11:43 -0400

    add each page to a Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import {Route, Switch} from "react-router-dom";
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
@@ -9,10 +9,13 @@ import Error from "./pages/Error";
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+      
     </>
   );
 }
