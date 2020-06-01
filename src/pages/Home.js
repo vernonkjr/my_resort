11fae29df99b1621a10e84fe6a14eb1a4828ab3a
vernonkjr <vkerrjr@gmail.com>
2020-06-01 17:39:44 -0400

    new Banner component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import {Link} from "react-router-dom";
 
 export default function Home() {
     return (
-        <Hero hero="defaultHero"></Hero>
+        <Hero hero="defaultHero">
+            <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                <Link to="/rooms" className="btn-primary">
+                    our rooms
+                </Link>
+            </Banner>
+        </Hero>
     )
 }
 
