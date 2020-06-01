657f56f97ce3348b4eee9d2452ade52e66441a6a
vernonkjr <vkerrjr@gmail.com>
2020-06-01 13:15:01 -0400

    create Hero and add to each page in app

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
+import Hero from "../components/Hero";
 
 export default function Error() {
     return (
-        <div>
-            hello from error page
-        </div>
+       <Hero />
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Hero from "../components/Hero";
 
 export default function Home() {
     return (
-        <div>
-            hello from home page
-        </div>
+        <Hero hero="defaultHero"></Hero>
     )
 }
 
