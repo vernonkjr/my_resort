95ea71ff517315fea2c7fc3adfc047026a533ae0
vernonkjr <vkerrjr@gmail.com>
2020-06-03 10:21:34 -0400

    Loading and Room component plus getting data for data.js

--- src/context.js
+++ src/context.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import items from "./data";
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
     state = {
-        greeting: "Hola",
-        name: "Sara"
+        rooms: [],
+        sortedRooms: [],
+        featuredRooms: [],
+        loading: true
     }
+
+    componentDidMount() {
+        let rooms = this.formtData(items);
+        let featuredRooms = rooms.filter(room => room.featured);
+        this.setState({
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
+            loading: false
+        })
+    }
+
+    formtData(items) {
+        let tempItems = items.map(item => {
+            let id = item.sys.id;
+            let images = item.fields.images.map(image => image.fields.file.url);
+            let room = {...item.fields, images, id};
+            return room;
+        });
+
+        return tempItems;
+    }
+        
+    
     render() {
         return (
             <RoomContext.Provider value={{...this.state}}>
